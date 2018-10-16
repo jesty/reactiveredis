820a173ac86b76cf2ff2f00e25dc208f92a70d6c
Davide Cerbo <davidecerbo@Davides-MBP.station>
2018-10-16 21:40:04 +0200

    refactoring

--- src/main/java/com/github/jesty/reactiveredis/TickerApiDelegateImpl.java
+++ src/main/java/com/github/jesty/reactiveredis/TickerApiDelegateImpl.java
@@ -3,11 +3,11 @@ package com.github.jesty.reactiveredis;
 import java.math.BigDecimal;
 import java.time.Duration;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerWebExchange;
 
-import com.github.jesty.reactiveredis.web.api.TickerApi;
 import com.github.jesty.reactiveredis.web.api.TickerApiDelegate;
 
 import reactor.core.publisher.Flux;
@@ -18,12 +18,25 @@ public class TickerApiDelegateImpl implements TickerApiDelegate {
 
 	@Override
 	public Mono<ResponseEntity<Flux<BigDecimal>>> tick(ServerWebExchange exchange) {
-		Flux<BigDecimal> myflux = Flux	.range(1, 10)
-										.map(n -> new BigDecimal(n))
-										.doOnNext(System.out::println)
-										.delayElements(Duration.ofSeconds(30));
+		Flux<BigDecimal> myflux = exchange	.getRequest()
+											.getHeaders()
+											.getAccept()
+											.stream()
+											.filter(mediaType -> mediaType.equals(MediaType.TEXT_EVENT_STREAM))
+											.findFirst()
+											.map(mt -> buildFlux())
+											.orElse(Flux.just(BigDecimal.ONE));
+
 		return Mono.just(ResponseEntity	.ok()
 										.body(myflux));
+
+	}
+
+	private Flux<BigDecimal> buildFlux() {
+		return Flux	.range(1, 10)
+					.map(n -> new BigDecimal(n))
+					.doOnNext(System.out::println)
+					.delayElements(Duration.ofSeconds(1));
 	}
 
 }
