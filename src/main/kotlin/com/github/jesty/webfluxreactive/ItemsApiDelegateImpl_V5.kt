276e2f83dd5d0b9934e9b476cf2b23f80a09c3b5
Davide Cerbo <davide.cerbo@alliance-healthcare.net>
2019-09-06 17:44:45 +0200

    Kotlin flow example

---
+++ src/main/kotlin/com/github/jesty/webfluxreactive/ItemsApiDelegateImpl_V5.kt
@@ -0,0 +1,60 @@
+package com.github.jesty.webfluxreactive
+
+import com.github.jesty.webfluxreactive.model.Item
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.reactive.flow.asFlow
+import org.springframework.data.redis.core.ReactiveRedisOperations
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ServerWebExchange
+import javax.annotation.PostConstruct
+
+@RestController
+@RequestMapping("/coroutine")
+@FlowPreview
+class ItemsApiDelegateImpl_V5(val itemOps: ReactiveRedisOperations<String, Item>) {
+
+
+    private lateinit var hot: Flow<Item>
+
+    @PostConstruct
+    fun startListener() {
+        hot = itemOps
+                .listenToChannel(Constants.TOPIC)
+                .doOnNext { next -> println("Next on ${next.channel}: ${next.message}") }
+                .map { it.message }
+                .flatMap { item ->
+                    itemOps
+                            .opsForList()
+                            .leftPush(Constants.ITEMS_LIST, item)
+                            .map { item }
+                            .doOnNext { println(it) }
+                }
+                .asFlow()
+
+    }
+
+    @RequestMapping(value = ["/items"], produces = ["text/event-stream"], method = [RequestMethod.GET])
+    fun listItems(filter: String?, exchange: ServerWebExchange?): Flow<Item> {
+        val fluxAll = itemOps
+                .opsForList()
+                .range(Constants.ITEMS_LIST, 0, -1)
+                .asFlow()
+
+        return flow {
+            fluxAll.collect { emit(it) }
+            hot.collect { emit(it) }
+        }.filter {
+            if (!filter.isNullOrEmpty()) it.name.contains(filter.toString()) else true
+        }
+    }
+
+}
+
+
+
